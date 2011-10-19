f95ec9231d51fc7a6cb56fdc261a6ff3db0d5f20
John Britton <public@johndbritton.com>
2011-10-19 03:44:27 +0200

    added category pages

---
+++ _plugins/category_page.rb
@@ -0,0 +1,33 @@
+module Jekyll
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_page.html')
+      self.data['category'] = category
+      category_title_prefix = site.config['category_title_prefix'] || 'Posts in &ldquo;'
+      category_title_suffix = site.config['category_title_suffix'] || '&rdquo;'
+      self.data['title'] = "#{category_title_prefix}#{category}#{category_title_suffix}"
+    end
+  end
+  class CategoryGenerator < Generator
+    safe true
+    def generate(site)
+      if site.layouts.key? 'category_page'
+        dir = site.config['category_dir'] || 'category'
+        site.categories.keys.each do |category|
+          write_category_page(site, File.join(dir, category), category)
+        end
+      end
+    end
+    def write_category_page(site, dir, category)
+      page = CategoryPage.new(site, site.source, dir, category)
+      page.render(site.layouts, site.site_payload)
+      page.write(site.dest)
+      site.pages << page
+    end
+  end
+end
